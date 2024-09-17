af089b00ef20233edb6ba743b2b510bb2231e9f1
vbobrik <valentina-bobrik1@yandex.by>
2024-09-17 11:51:44 +0300

    add input form test

--- tests/yandexPage.spec.js
+++ tests/yandexPage.spec.js
@@ -1,21 +1,24 @@
 const { test, expect } = require('@playwright/test');
 const URL = 'https://translate.yandex.com/';
 
-// test.beforeEach(async ({ page }) => {
-//     await page.goto(URL);
-// })
+test.beforeEach(async ({ page }) => {
+    await page.goto(URL);
+})
 
-// test.describe('Word translation', () => {
+test.describe('Word translation', () => {
+    test('Input form presents on the page', async ({ page }) => {
+        let field =  page.getByLabel('Start typing or insert a link').first();
+        await expect(field).toBeVisible();
+        await expect(page.locator("//*[@accept='image/*']/parent::div")).toBeVisible();
+    })
     test('@Yandex Translate English word', async ({ page }) => {
         await page.goto(URL);
-        // let field = page.getByLabel('fakeArea');
-        let field =  page.locator('#srcLabel');
-        await field.click();
-        // await expect(page).toHaveTitle(/translate/);
+        let field =  page.getByLabel('Start typing or insert a link').first();
+     
         await field.fill('words');
         await field.press('Enter');
 
-        let translatedField = page.getById('dstBox');
+        let translatedField = page.locator('#dstBox>div');
         await expect(translatedField.getByText('слова')).toBeVisible();
     })
-// });
+});
